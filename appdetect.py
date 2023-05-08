dc5cd29256ed79d0610111a9426250769feed29b
Bouafia Adam <74255275+Modjo-Djodjo@users.noreply.github.com>
2023-05-08 14:57:36 +0100

    Update appdetect.py

--- appdetect.py
+++ appdetect.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/detect', methods=['POST'])
 def detect_language():
-    text = request.json['text']
+    data = request.get_json()
+    if 'text' not in data:
+        return jsonify({'error': 'Missing text field in request'})
+    text = data['text']
     detected_lang = detect(text)
     if detected_lang in ['ar', 'fr', 'en']:
         return jsonify({'language': detected_lang})
