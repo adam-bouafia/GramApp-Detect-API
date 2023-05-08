dc304fb141142875c97fb9b8b760ab40cb54886c
Bouafia Adam <74255275+Modjo-Djodjo@users.noreply.github.com>
2023-05-08 13:46:08 +0100

    Update appdetect.py

--- appdetect.py
+++ appdetect.py
@@ -1,18 +1,16 @@
 from flask import Flask, request, jsonify
-from langdetect import detect_langs
+from langdetect import detect
 
 app = Flask(__name__)
 
 @app.route('/detect', methods=['POST'])
 def detect_language():
     text = request.json['text']
-    langs = detect_langs(text)
-    detected_lang = ''
-    for lang in langs:
-        if lang.lang in ['ar', 'fr', 'en']:
-            detected_lang = lang.lang
-            break
-    return jsonify({'language': detected_lang})
+    detected_lang = detect(text)
+    if detected_lang in ['ar', 'fr', 'en']:
+        return jsonify({'language': detected_lang})
+    else:
+        return jsonify({'error': 'Language not supported'})
 
 if __name__ == '__main__':
     app.run()
